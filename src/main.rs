9d9f615221385d00883c9ab36c491f38dabdf58c
miso01 <misosvec01@gmail.com>
2022-10-27 15:43:35 +0200

    generate all string rotations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    
+    //println!("{:?}", string_rotations(String::from("Ahoj")));
+}
+
+fn string_rotations(str: String) -> Vec<String> {
+    let mut rotations: Vec<String> = vec![];
+    let str_len = str.len();
+    for delimiter in 0..str_len {
+        let (prefix, sufix) = str.split_at(delimiter);
+        let result = [
+            prefix.chars().rev().collect::<String>(),
+            sufix.chars().rev().collect::<String>(),
+        ]
+        .join("");
+        rotations.push(result.chars().rev().collect::<String>());
+    }
+    return rotations;
 }
