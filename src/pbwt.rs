7664e0bc55533d14d0965d96c7775c12c06f6609
miso01 <misosvec01@gmail.com>
2023-01-16 21:37:57 +0100

    report set maximal matches algorithm

--- src/pbwt.rs
+++ src/pbwt.rs
@@ -122,11 +122,107 @@ fn report_long_matches(
     return result;
 }
 
+fn report_set_maximal_matches(
+    haplotypes: Vec<Vec<u32>>,
+    match_length: u32,
+) -> Vec<(u32, u32, u32, u32)> {
+    let mut result: Vec<(u32, u32, u32, u32)> = vec![];
+    let haplotype_length = haplotypes[0].len();
+    let mut ppa: Vec<u32> = (0..haplotypes.len() as u32).collect();
+    let mut div: Vec<u32> = vec![0; haplotypes.len()];
+
+    for k in 0..haplotype_length {
+        div.push((k + 1) as u32);
+
+        for i in 0..haplotypes.len() {
+            let mut m = i as i32 - 1;
+            let mut n = i as u32 + 1;
+            let mut match_continues = false;
+
+            if div[i] <= div[i + 1] {
+                while (m + 1) >= 0 && div[(m + 1) as usize] <= div[i] {
+                    if m >= 0
+                        && haplotypes[ppa[m as usize] as usize][k] == haplotypes[ppa[i] as usize][k]
+                    {
+                        match_continues = true;
+                        break;
+                    }
+                    m -= 1;
+                }
+            }
+
+            if match_continues {
+                continue;
+            }
+
+            if div[i] >= div[i + 1] {
+                while div[n as usize] <= div[i + 1] {
+                    if haplotypes[ppa[n as usize] as usize][k] == haplotypes[ppa[i] as usize][k] {
+                        match_continues = true;
+                        break;
+                    }
+                    n += 1;
+                }
+            }
+
+            if match_continues {
+                continue;
+            }
+
+            for j in ((m + 1) as usize)..i as usize {
+                if div[i] < k as u32 {
+                    result.push((ppa[i], ppa[j], div[i], k as u32))
+                }
+            }
+
+            for j in i + 1..n as usize {
+                if div[i + 1] < k as u32 {
+                    result.push((ppa[i], ppa[j], div[i + 1], k as u32))
+                }
+            }
+        }
+        if k < haplotype_length {
+            let mut a: Vec<u32> = vec![];
+            let mut b: Vec<u32> = vec![];
+            let mut d: Vec<u32> = vec![];
+            let mut e: Vec<u32> = vec![];
+            let mut p: u32 = k as u32 + 1;
+            let mut q: u32 = k as u32 + 1;
+
+            for (index, match_start) in ppa.iter().zip(div.iter()) {
+                let haplotype = &haplotypes[*index as usize];
+                let allele = haplotype[k];
+
+                if *match_start > p {
+                    p = *match_start;
+                }
+                if *match_start > q {
+                    q = *match_start;
+                }
+
+                if allele == 0 {
+                    a.push(*index as u32);
+                    d.push(p);
+                    p = 0;
+                } else {
+                    b.push(*index as u32);
+                    e.push(q);
+                    q = 0;
+                }
+            }
+            ppa = [a, b].concat();
+            div = [d, e].concat();
+        }
+    }
+    return result;
+}
+
 #[cfg(test)]
 mod tests {
     use crate::pbwt::build_prefix_and_divergence_array;
     use crate::pbwt::build_prefix_array;
     use crate::pbwt::report_long_matches;
+    use crate::pbwt::report_set_maximal_matches;
 
     #[test]
     fn test_prefix_array() {
@@ -178,6 +274,43 @@ mod tests {
                 (4, vec![0], vec![7]),
                 (5, vec![4], vec![1, 6]),
                 (5, vec![3], vec![2])
+            ]
+        );
+    }
+
+    #[test]
+    fn test_report_set_maximal_matches() {
+        let haplotypes: Vec<Vec<u32>> = vec![
+            vec![0, 1, 0, 1, 0, 1],
+            vec![1, 1, 0, 0, 0, 1],
+            vec![1, 1, 1, 1, 1, 1],
+            vec![0, 1, 1, 1, 1, 0],
+            vec![0, 0, 0, 0, 0, 0],
+            vec![1, 0, 0, 0, 1, 0],
+            vec![1, 1, 0, 0, 0, 1],
+            vec![0, 1, 0, 1, 1, 0],
+        ];
+        assert_eq!(
+            report_set_maximal_matches(haplotypes, 3),
+            vec![
+                (4, 0, 0, 1),
+                (4, 3, 0, 1),
+                (4, 7, 0, 1),
+                (5, 1, 0, 1),
+                (5, 2, 0, 1),
+                (5, 6, 0, 1),
+                (3, 0, 0, 2),
+                (3, 7, 0, 2),
+                (2, 1, 0, 2),
+                (2, 6, 0, 2),
+                (4, 5, 1, 4),
+                (5, 4, 1, 4),
+                (0, 7, 0, 4),
+                (7, 0, 0, 4),
+                (4, 1, 2, 5),
+                (4, 6, 2, 5),
+                (3, 2, 1, 5),
+                (2, 3, 1, 5)
             ]
         );
     }
