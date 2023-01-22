88e2fad0db87e91e86b58bf1b8ef03601b399a74
miso01 <misosvec01@gmail.com>
2023-01-22 10:25:42 +0100

    mprvoed

--- src/pbwt.rs
+++ src/pbwt.rs
@@ -1,19 +1,18 @@
-fn build_prefix_array(haplotypes: Vec<Vec<u32>>) -> Vec<u32> {
-    let haplotype_length = haplotypes[0].len();
-    let mut ppa: Vec<u32> = (0..haplotypes.len() as u32).collect();
+fn build_prefix_array(haplotypes: Vec<Vec<u8>>) -> Vec<usize> {
+    let mut ppa: Vec<usize> = (0..haplotypes.len()).collect();
 
-    for k in 0..haplotype_length - 1 {
-        let mut a: Vec<u32> = vec![];
-        let mut b: Vec<u32> = vec![];
+    for k in 0..haplotypes[0].len() - 1 {
+        let mut a: Vec<usize> = vec![];
+        let mut b: Vec<usize> = vec![];
 
         for i in ppa {
-            let haplotype = &haplotypes[i as usize];
+            let haplotype = &haplotypes[i];
             let allele = haplotype[k];
 
             if allele == 0 {
-                a.push(i as u32);
+                a.push(i);
             } else {
-                b.push(i as u32);
+                b.push(i);
             }
         }
         ppa = [a, b].concat();
@@ -21,21 +20,20 @@ fn build_prefix_array(haplotypes: Vec<Vec<u32>>) -> Vec<u32> {
     return ppa;
 }
 
-fn build_prefix_and_divergence_array(haplotypes: Vec<Vec<u32>>) -> Vec<u32> {
-    let haplotype_length = haplotypes[0].len();
-    let mut ppa: Vec<u32> = (0..haplotypes.len() as u32).collect();
-    let mut div: Vec<u32> = vec![0; haplotypes.len()];
+fn build_prefix_and_divergence_array(haplotypes: Vec<Vec<u8>>) -> Vec<usize> {
+    let mut ppa: Vec<usize> = (0..haplotypes.len()).collect();
+    let mut div: Vec<usize> = vec![0; haplotypes.len()];
 
-    for k in 0..haplotype_length - 1 {
-        let mut a: Vec<u32> = vec![];
-        let mut b: Vec<u32> = vec![];
-        let mut d: Vec<u32> = vec![];
-        let mut e: Vec<u32> = vec![];
-        let mut p: u32 = k as u32 + 1;
-        let mut q: u32 = k as u32 + 1;
+    for k in 0..haplotypes[0].len()-1{
+        let mut a: Vec<usize> = vec![];
+        let mut b: Vec<usize> = vec![];
+        let mut d: Vec<usize> = vec![];
+        let mut e: Vec<usize> = vec![];
+        let mut p: usize = k + 1;
+        let mut q: usize = k + 1;
 
         for (index, match_start) in ppa.iter().zip(div.iter()) {
-            let haplotype = &haplotypes[*index as usize];
+            let haplotype = &haplotypes[*index];
             let allele = haplotype[k];
 
             if *match_start > p {
@@ -46,11 +44,11 @@ fn build_prefix_and_divergence_array(haplotypes: Vec<Vec<u32>>) -> Vec<u32> {
             }
 
             if allele == 0 {
-                a.push(*index as u32);
+                a.push(*index);
                 d.push(p);
                 p = 0;
             } else {
-                b.push(*index as u32);
+                b.push(*index);
                 e.push(q);
                 q = 0;
             }
@@ -62,23 +60,22 @@ fn build_prefix_and_divergence_array(haplotypes: Vec<Vec<u32>>) -> Vec<u32> {
 }
 
 fn report_long_matches(
-    haplotypes: Vec<Vec<u32>>,
-    match_length: u32,
-) -> Vec<(usize, Vec<u32>, Vec<u32>)> {
-    let mut result: Vec<(usize, Vec<u32>, Vec<u32>)> = vec![];
-    let haplotype_length = haplotypes[0].len();
-    let mut ppa: Vec<u32> = (0..haplotypes.len() as u32).collect();
-    let mut div: Vec<u32> = vec![0; haplotypes.len()];
+    haplotypes: Vec<Vec<u8>>,
+    match_length: usize,
+) -> Vec<(usize, Vec<usize>, Vec<usize>)> {
+    let mut result: Vec<(usize, Vec<usize>, Vec<usize>)> = vec![];
+    let mut ppa: Vec<usize> = (0..haplotypes.len()).collect();
+    let mut div: Vec<usize> = vec![0; haplotypes.len()];
 
-    for k in 0..haplotype_length {
-        let mut a: Vec<u32> = vec![];
-        let mut b: Vec<u32> = vec![];
-        let mut d: Vec<u32> = vec![];
-        let mut e: Vec<u32> = vec![];
-        let mut p: u32 = k as u32 + 1;
-        let mut q: u32 = k as u32 + 1;
-        let mut ma: Vec<u32> = vec![];
-        let mut mb: Vec<u32> = vec![];
+    for k in 0..haplotypes[0].len() {
+        let mut a = vec![];
+        let mut b = vec![];
+        let mut d = vec![];
+        let mut e = vec![];
+        let mut p = k + 1;
+        let mut q = k + 1;
+        let mut ma = vec![];
+        let mut mb = vec![];
 
         for (index, match_start) in ppa.iter().zip(div.iter()) {
             if *match_start as i32 > ((k as i32) - match_length as i32) {
@@ -88,7 +85,7 @@ fn report_long_matches(
                 ma = vec![];
                 mb = vec![];
             }
-            let haplotype = &haplotypes[*index as usize];
+            let haplotype = &haplotypes[*index];
             let allele = haplotype[k];
 
             if *match_start > p {
@@ -99,22 +96,22 @@ fn report_long_matches(
             }
 
             if allele == 0 {
-                a.push(*index as u32);
+                a.push(*index);
                 d.push(p);
                 p = 0;
-                ma.push(*index as u32);
+                ma.push(*index);
             } else {
-                b.push(*index as u32);
+                b.push(*index);
                 e.push(q);
                 q = 0;
-                mb.push(*index as u32)
+                mb.push(*index)
             }
         }
         if !ma.is_empty() && !mb.is_empty() {
             result.push((k, ma, mb));
         }
 
-        if k < (haplotype_length - (1 as usize)) {
+        if k < (haplotypes[0].len() - 1) {
             ppa = [a, b].concat();
             div = [d, e].concat();
         }
@@ -123,16 +120,15 @@ fn report_long_matches(
 }
 
 fn report_set_maximal_matches(
-    haplotypes: Vec<Vec<u32>>,
-    match_length: u32,
-) -> Vec<(u32, u32, u32, u32)> {
-    let mut result: Vec<(u32, u32, u32, u32)> = vec![];
+    haplotypes: Vec<Vec<u8>>,
+) -> Vec<(usize, usize, usize, usize)> {
+    let mut result: Vec<(usize, usize, usize, usize)> = vec![];
     let haplotype_length = haplotypes[0].len();
-    let mut ppa: Vec<u32> = (0..haplotypes.len() as u32).collect();
-    let mut div: Vec<u32> = vec![0; haplotypes.len()];
+    let mut ppa: Vec<usize> = (0..haplotypes.len()).collect();
+    let mut div = vec![0; haplotypes.len()];
 
     for k in 0..haplotype_length {
-        div.push((k + 1) as u32);
+        div.push((k + 1));
 
         for i in 0..haplotypes.len() {
             let mut m = i as i32 - 1;
@@ -170,27 +166,27 @@ fn report_set_maximal_matches(
             }
 
             for j in ((m + 1) as usize)..i as usize {
-                if div[i] < k as u32 {
-                    result.push((ppa[i], ppa[j], div[i], k as u32))
+                if div[i] < k {
+                    result.push((ppa[i], ppa[j], div[i], k))
                 }
             }
 
             for j in i + 1..n as usize {
-                if div[i + 1] < k as u32 {
-                    result.push((ppa[i], ppa[j], div[i + 1], k as u32))
+                if div[i + 1] < k {
+                    result.push((ppa[i], ppa[j], div[i + 1], k))
                 }
             }
         }
         if k < haplotype_length {
-            let mut a: Vec<u32> = vec![];
-            let mut b: Vec<u32> = vec![];
-            let mut d: Vec<u32> = vec![];
-            let mut e: Vec<u32> = vec![];
-            let mut p: u32 = k as u32 + 1;
-            let mut q: u32 = k as u32 + 1;
+            let mut a = vec![];
+            let mut b = vec![];
+            let mut d = vec![];
+            let mut e = vec![];
+            let mut p = k + 1;
+            let mut q = k + 1;
 
             for (index, match_start) in ppa.iter().zip(div.iter()) {
-                let haplotype = &haplotypes[*index as usize];
+                let haplotype = &haplotypes[*index];
                 let allele = haplotype[k];
 
                 if *match_start > p {
@@ -201,11 +197,11 @@ fn report_set_maximal_matches(
                 }
 
                 if allele == 0 {
-                    a.push(*index as u32);
+                    a.push(*index);
                     d.push(p);
                     p = 0;
                 } else {
-                    b.push(*index as u32);
+                    b.push(*index);
                     e.push(q);
                     q = 0;
                 }
@@ -219,79 +215,56 @@ fn report_set_maximal_matches(
 
 #[cfg(test)]
 mod tests {
-    use crate::pbwt::build_prefix_and_divergence_array;
     use crate::pbwt::build_prefix_array;
     use crate::pbwt::report_long_matches;
     use crate::pbwt::report_set_maximal_matches;
+    use crate::pbwt::build_prefix_and_divergence_array;
+
+
+    fn get_haplotypes() -> Vec<Vec<u8>> {
+        return vec![
+            vec![0, 1, 0, 1, 0, 1],
+            vec![1, 1, 0, 0, 0, 1],
+            vec![1, 1, 1, 1, 1, 1],
+            vec![0, 1, 1, 1, 1, 0],
+            vec![0, 0, 0, 0, 0, 0],
+            vec![1, 0, 0, 0, 1, 0],
+            vec![1, 1, 0, 0, 0, 1],
+            vec![0, 1, 0, 1, 1, 0],
+        ];
+    }
+
 
     #[test]
     fn test_prefix_array() {
-        let haplotypes: Vec<Vec<u32>> = vec![
-            vec![0, 1, 0, 1, 0, 1],
-            vec![1, 1, 0, 0, 0, 1],
-            vec![1, 1, 1, 1, 1, 1],
-            vec![0, 1, 1, 1, 1, 0],
-            vec![0, 0, 0, 0, 0, 0],
-            vec![1, 0, 0, 0, 1, 0],
-            vec![1, 1, 0, 0, 0, 1],
-            vec![0, 1, 0, 1, 1, 0],
-        ];
-        assert_eq!(build_prefix_array(haplotypes), vec![4, 1, 6, 0, 5, 7, 3, 2]);
+        assert_eq!(build_prefix_array(get_haplotypes()), vec![4, 1, 6, 0, 5, 7, 3, 2]);
     }
+
     #[test]
     fn test_build_prefix_and_divergence_array() {
-        let haplotypes: Vec<Vec<u32>> = vec![
-            vec![0, 1, 0, 1, 0, 1],
-            vec![1, 1, 0, 0, 0, 1],
-            vec![1, 1, 1, 1, 1, 1],
-            vec![0, 1, 1, 1, 1, 0],
-            vec![0, 0, 0, 0, 0, 0],
-            vec![1, 0, 0, 0, 1, 0],
-            vec![1, 1, 0, 0, 0, 1],
-            vec![0, 1, 0, 1, 1, 0],
-        ];
         assert_eq!(
-            build_prefix_and_divergence_array(haplotypes),
+            build_prefix_and_divergence_array(get_haplotypes()),
             vec![5, 2, 0, 4, 5, 4, 3, 1]
         );
     }
+
     #[test]
     fn test_report_matches() {
-        let haplotypes: Vec<Vec<u32>> = vec![
-            vec![0, 1, 0, 1, 0, 1],
-            vec![1, 1, 0, 0, 0, 1],
-            vec![1, 1, 1, 1, 1, 1],
-            vec![0, 1, 1, 1, 1, 0],
-            vec![0, 0, 0, 0, 0, 0],
-            vec![1, 0, 0, 0, 1, 0],
-            vec![1, 1, 0, 0, 0, 1],
-            vec![0, 1, 0, 1, 1, 0],
-        ];
         assert_eq!(
-            report_long_matches(haplotypes, 3),
+            report_long_matches(get_haplotypes(), 3),
             vec![
                 (4, vec![4], vec![5]),
                 (4, vec![0], vec![7]),
                 (5, vec![4], vec![1, 6]),
-                (5, vec![3], vec![2])
+                (5, vec![3], vec![2]),
             ]
         );
     }
 
     #[test]
     fn test_report_set_maximal_matches() {
-        let haplotypes: Vec<Vec<u32>> = vec![
-            vec![0, 1, 0, 1, 0, 1],
-            vec![1, 1, 0, 0, 0, 1],
-            vec![1, 1, 1, 1, 1, 1],
-            vec![0, 1, 1, 1, 1, 0],
-            vec![0, 0, 0, 0, 0, 0],
-            vec![1, 0, 0, 0, 1, 0],
-            vec![1, 1, 0, 0, 0, 1],
-            vec![0, 1, 0, 1, 1, 0],
-        ];
         assert_eq!(
-            report_set_maximal_matches(haplotypes, 3),
+            report_set_maximal_matches(get_haplotypes()),
             vec![
                 (4, 0, 0, 1),
                 (4, 3, 0, 1),
@@ -310,7 +283,7 @@ mod tests {
                 (4, 1, 2, 5),
                 (4, 6, 2, 5),
                 (3, 2, 1, 5),
-                (2, 3, 1, 5)
+                (2, 3, 1, 5),
             ]
         );
     }
