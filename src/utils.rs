55ec6bc10f127de0c6a8100eb56cbb199c9d7f0a
miso01 <misosvec01@gmail.com>
2023-01-27 17:46:59 +0100

    write to file and encode haplotypes functions

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use std::io::prelude::*;
 use rand::Rng;
 
 pub fn generate_haplotypes(count: usize, variable_sites: usize) -> Vec<Vec<u8>> {
@@ -12,4 +14,24 @@ pub fn generate_haplotypes(count: usize, variable_sites: usize) -> Vec<Vec<u8>> 
         haplotypes.push(haplotype);
     }
     return haplotypes;
+}
+
+pub fn write_to_file(data: &str, output_file: &str) {
+    File::create(output_file).unwrap().write(data.as_ref()).unwrap();
+}
+
+pub fn encode_haplotypes(haplotypes: Vec<Vec<u8>>) -> String {
+    let mut result: String = haplotypes
+        .into_iter()
+        .map(|haplotype| {
+            let mut encoded_haplotype = haplotype.into_iter().enumerate().map(|(i, allele)| {
+                let mut str = i.to_string();
+                str.push(allele.to_string().chars().last().unwrap());
+                return str;
+            }).collect::<String>();
+            encoded_haplotype += "#";
+            return encoded_haplotype;
+        }).collect();
+    result.remove(result.len() - 1);
+    return result;
 }
